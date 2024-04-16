08d7b6c8cb2de36ece43c1b831401008bbcd1eb8
julia <julia.zhyvylo@asila.mk>
2024-04-16 21:03:58 +0200

    add exeption

--- task_1.py
+++ task_1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-  current_date = datetime.now()
-  date_obj = datetime.strptime(date, "%Y-%m-%d")
-  diff = current_date - date_obj
-  return diff.days
+  try:
+    current_date = datetime.now()
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+    diff = current_date - date_obj
+    return diff.days
+  except ValueError:
+    print("Wrong date format")
+  
 
-result = get_days_from_today("2024-10-09")
+result = get_days_from_today("10.09.2023")
 print(result)
