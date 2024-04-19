79abc0c6d715b7708677c9a93c289d963ffcf27a
Julia <zhyvka89@gmail.com>
2024-04-19 19:37:42 +0200

    code clean up

--- task_2.py
+++ task_2.py
@@ -1,17 +1,17 @@
 import random
 
-
 def get_numbers_ticket(min, max, quantity):
-  try:
-    if (min >= 1 and max <= 1000):
-      range_of_numbers = range(min, max)
-      random_numbers = random.sample(range_of_numbers, k=quantity)
-      random_numbers.sort()
-      return random_numbers
-    else:
-      return []
-  except:
-    print('Quantity must be smaller than max number')  
+    try:
+        if (min >= 1 and max <= 1000):
+            range_of_numbers = range(min, max)
+            random_numbers = random.sample(range_of_numbers, k=quantity)
+            random_numbers.sort()
+            return random_numbers
+        else:
+            return []
+    except:
+        print('Quantity must be smaller than max number')
 
-result = get_numbers_ticket(1, 1000, 9)
+
+result = get_numbers_ticket(1, 500, 9)
 print(result)
--- task_4.py
+++ task_4.py
@@ -1,35 +1,6 @@
 from datetime import datetime, timedelta
 
 
-# def get_upcoming_birthdays(users):
-#   current_date = datetime.today().date()
-#   list_of_upcoming_bdays = []
-#   for user in users:
-#     bday_obj = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
-#     bday_day = bday_obj.day
-#     bday_month = bday_obj.month
-#     bday_this_year = datetime(
-#       year=current_date.year, month=bday_month, day=bday_day).date()
-#     if (bday_this_year < current_date):
-#       bday_next_year = datetime(
-#         year=current_date.year+1, month=bday_month, day=bday_day).date()
-#       user_obj = {
-#         "name": user["name"],
-#         "congratulation_date": bday_next_year.strftime("%Y.%m.%d")
-#       }
-#       list_of_upcoming_bdays.append(user_obj)
-#     else:
-#       diff = bday_this_year - current_date
-#       time_delta = timedelta(7)
-#       if (diff <= time_delta):
-#         user_obj = {
-#           "name": user["name"],
-#           "congratulation_date": bday_this_year.strftime("%Y.%m.%d")
-#         }
-#         list_of_upcoming_bdays.append(user_obj)
-#         print("We will celebrate this week!!!")
-#     print(list_of_upcoming_bdays)
-
 def get_congrat_day(date):
   weekday = date.weekday()
   congrat_day = date
@@ -74,7 +45,6 @@ def get_upcoming_birthdays(users):
   return list_of_upcoming_bdays
 
 
-
 users = [
   {"name": "John Doe", "birthday": "1985.04.20"},
   {"name": "Julia Brook", "birthday": "1995.04.22"},
@@ -83,8 +53,3 @@ users = [
 
 
 get_upcoming_birthdays(users)
-
-
-# now = datetime.now()
-# future_date = now + timedelta(days=10)  # Додаємо 10 днів до поточної дати
-# print(future_date)
